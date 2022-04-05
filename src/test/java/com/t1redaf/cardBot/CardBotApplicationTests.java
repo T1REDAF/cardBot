bf6b97e484b0719ba09bdf825a78d9e8e3ae0f8d
DinY <dinar5440@bk.ru>
2022-04-05 23:55:12 +0500

    Initial commit

---
+++ src/test/java/com/t1redaf/cardBot/CardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1redaf.cardBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
